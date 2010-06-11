8100a7f60ee5ddf2ad29e031ef8e7d204a648525
Jeremy Orem <jeremy.orem@gmail.com>
2010-06-11 16:50:44 -0700

    Added:     show_commands     selectable reload/restart

--- src/httpdmultiuser/commands.py
+++ src/httpdmultiuser/commands.py
@@ -5,11 +5,11 @@ from . import apache
 
 
 commands = {}
-def command(options=None):
+def command(options=None, usage=''):
     if not options:
         options = []
     def wrapper(f):
-        parser = OptionParser()
+        parser = OptionParser(usage=usage % {'f': f.__name__})
         for o in options:
             parser.add_option(o)
 
@@ -24,11 +24,25 @@ def command(options=None):
     return wrapper
     
 
-@command
+@command(usage='usage: %%prog %(f)s {all|appname appname}')
 def restart(opts, *args):
     for a in apache.all_apaches():
-        a.restart()
+        if 'all' in args or a.name in args:
+            a.restart()
+
 
 @command([make_option("-s", "--sort", default='name')])
 def report(opts, *args):
     apache.print_report(apache.all_apaches(), sort=opts.sort)
+
+
+@command(usage='usage: %%prog %(f)s {all|appname appname}')
+def reload(opts, *args):
+    for a in apache.all_apaches():
+        if 'all' in args or a.name in args:
+            a.restart()
+
+@command()
+def show_commands(*args):
+    for c in commands:
+        print c
